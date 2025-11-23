7867a02feaa6b3f6fd72e131e4a6d27c0b106e2a
mtkpapa <ebanashka10028@gmail.com>
2025-11-23 13:09:11 +0300

    fixup! sched/fair: Remove distribute_running from CFS bandwidth
    https://github.com/mtkpapa/kernel_xiaomi_sm8250_mod/commit/7cd70a40511dd83de3ce15bd58061242fa00243d
    Signed-off-by: mtkpapa <ebanashka10028@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5352,7 +5352,6 @@ static int do_sched_cfs_period_timer(struct cfs_bandwidth *cfs_b, int overrun)
 	 * This check is repeated as we release cfs_b->lock while we unthrottle.
 	 */
 	while (throttled && cfs_b->runtime > 0) {
-		cfs_b->distribute_running = 1;
 		raw_spin_unlock(&cfs_b->lock);
 		/* we can't nest cfs_b->lock while distributing bandwidth */
 		distribute_cfs_runtime(cfs_b);
